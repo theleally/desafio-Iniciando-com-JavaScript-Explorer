96a21cda6bc277e3a0119b3735d7ad1a2e0bfd81
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2023-05-08 11:57:15 -0300

    desafio finalizado

---
+++ main.js
@@ -0,0 +1,61 @@
+
+//  alert("Hello World!");
+
+//  let numberOne = 2;
+//  let numberTwo = 3;
+//  let result = numberOne + numberTwo;
+//  alert(result);
+
+//  let n = "2";
+
+//  if (n === 2) {
+//    alert("É um número");
+//  } else {
+//    alert("Não é um número");
+//  }
+
+//  let string = 2;
+
+//  if (string === "2") {
+//    alert("É uma string");
+//  } else {
+//    alert("Não é uma string")
+//  }
+
+//  let booleano = 2;
+
+//  if (booleano === true || false) {
+//    alert("É um booleano");
+//  } else {
+//    alert("Não é um booleano");
+//  }
+
+//  let n1 = 10;
+//  let n2 = 5;
+//  let result = n1 - n2;
+//  alert(result);
+
+//  let n1 = 10;
+//  let n2 = 5;
+//  let result = n1 * n2;
+//  alert(result);
+
+//  let n1 = 10;
+//  let n2 = 5;
+//  let result = n1 / n2;
+//  alert(result);
+
+//  let number = 10;
+
+//  if (number % 2 === 0) {
+//    alert("É um número par");
+//  } else {
+//    alert("Não é um número par");
+//  }
+
+let number = 6;
+if (number % 2 !== 0) {
+  alert("É um número ímpar");
+} else {
+  alert("Não é um número ímpar");
+}
